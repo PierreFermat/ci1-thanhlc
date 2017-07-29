c7339b813410e11b4463914e24779e72ccf86c85
PierreFermat <Thanhcls1316@gmail.com>
2017-07-29 20:40:17 +0700

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,13 @@
+import touhou.GameWindow;
+
+/**
+ * Created by huynq on 7/4/17.
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        GameWindow gameWindow = new GameWindow();
+        System.out.println("Starting loop");
+        gameWindow.loop();
+    }
+}

e8766fb317c4280f9303727c41c4ae44c922c985
PierreFermat <Thanhcls1316@gmail.com>
2017-08-02 18:47:41 +0700

    02.08

--- src/touhou/GameWindow.java
+++ src/touhou/GameWindow.java
@@ -19,10 +19,14 @@ public class GameWindow extends Frame {
     private BufferedImage backBufferImage;
     private BufferedImage background;
     private BufferedImage player_Straight;
+    private BufferedImage player_Right;
+    private BufferedImage player_Left;
+
     private int playX = 178;
     private int playY = 600;
     private int dx ;
     private int dy ;
+    private int i;
 
 
     private int backgroundy = -2400;
@@ -31,6 +35,8 @@ public class GameWindow extends Frame {
     public GameWindow() {
         background = tklibs.SpriteUtils.loadImage("assets/images/background/0.png");
         player_Straight = tklibs.SpriteUtils.loadImage("assets/images/players/straight/0.png");
+        player_Left = tklibs.SpriteUtils.loadImage("assets/images/players/left/0.png");
+        player_Right = tklibs.SpriteUtils.loadImage("assets/images/players/right/0.png");
         setupGameLoop();
         setupWindow();
     }
@@ -63,15 +69,19 @@ public class GameWindow extends Frame {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_RIGHT){
                     dx = 4;
+                    i = 1;
                 }
                 if (e.getKeyCode() == KeyEvent.VK_LEFT){
                     dx = -4;
+                    i = 2;
                 }
                 if (e.getKeyCode() == KeyEvent.VK_UP){
                     dy = -4;
+                    i =3;
                 }
                 if (e.getKeyCode() == KeyEvent.VK_DOWN){
                     dy = 4;
+                    i=3;
                 }
 
             }
@@ -80,6 +90,7 @@ public class GameWindow extends Frame {
             public void keyReleased(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_RIGHT){
                     dx = 0;
+
                 }
                 if (e.getKeyCode() == KeyEvent.VK_LEFT){
                     dx = 0;
@@ -125,8 +136,8 @@ public class GameWindow extends Frame {
         if ( playY > 740){
             playY = 740;
         }
-        if (playX > 380) {
-            playX = 380;
+        if (playX > 365) {
+            playX = 365;
         }
 
     }
@@ -137,7 +148,19 @@ public class GameWindow extends Frame {
         backBufferGraphics.fillRect(0,0,1024,768);
         backBufferGraphics.drawImage(background,0,backgroundy,null);
         backgroundy += 1;
-        backBufferGraphics.drawImage(player_Straight, playX,playY ,null);
+        if (backgroundy > 0) {
+            backgroundy = 0;
+        }
+        if (i==1){
+            backBufferGraphics.drawImage(player_Right, playX,playY ,null);
+        }
+        if (i==2){
+            backBufferGraphics.drawImage(player_Left, playX,playY ,null);
+        }
+        if (i==3){
+            backBufferGraphics.drawImage(player_Straight , playX,playY ,null);
+        }
+
         g.drawImage(backBufferImage, 0, 0, null);
     }
 
